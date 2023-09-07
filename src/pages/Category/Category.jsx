1a281d60971f84cb02dc18bdfc9573de45aef98b
mafizul24h <mafizul24h@gmail.com>
2023-09-07 07:52:13 +0600

    Category News Filtering

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -4,25 +4,25 @@ import CardNews from './CardNews';
 import { AuthContext } from '../../providers/AuthProvider';
 
 const Category = () => {
-    const categoryNes = useLoaderData();
-    const { news } = useContext(AuthContext);
-    const [categoryNews, setCategoryNews] = useState(news);
+    const categoryNews = useLoaderData();
+    // const { news } = useContext(AuthContext);
+    // const [categoryNews, setCategoryNews] = useState(news);
     // console.log(news);
     // console.log(categoryNews);
 
-    useEffect(() => {
-        if (categoryNes.id == 0) {
-            setCategoryNews(news);
-        } else {
-            const newsCategory = news.filter(n => n.category == categoryNes.id);
-            setCategoryNews(newsCategory)
-        }
-    }, [categoryNes, news])
+    // useEffect(() => {
+    //     if (categoryNes.id == 0) {
+    //         setCategoryNews(news);
+    //     } else {
+    //         const newsCategory = news.filter(n => n.category == categoryNes.id);
+    //         setCategoryNews(newsCategory)
+    //     }
+    // }, [categoryNes, news])
 
     return (
         <div>
             {
-                categoryNews.map(news => <CardNews key={news._id} news={news} />)
+                categoryNews?.map(news => <CardNews key={news._id} news={news} />)
             }
         </div>
     );
